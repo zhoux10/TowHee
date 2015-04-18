f39a454dd4b9bd2b55cffc3d977aaf4dc60d83b1
suprita shankar <suprita.shankar@gmail.com>
2015-04-18 12:53:03 -0700

    add events

---
+++ public/modules/events/config/events.client.routes.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Setting up route
+angular.module('articles').config(['$stateProvider',
+	function($stateProvider) {
+		// Articles state routing
+		$stateProvider.
+		state('listEvents', {
+			url: '/events',
+			templateUrl: 'modules/events/views/list-events.client.view.html'
+		}).
+		state('createEvent', {
+			url: '/events/create',
+			templateUrl: 'modules/events/views/create-event.client.view.html'
+		});
+		/*
+		state('viewArticle', {
+			url: '/articles/:articleId',
+			templateUrl: 'modules/articles/views/view-article.client.view.html'
+		}).
+		state('editArticle', {
+			url: '/articles/:articleId/edit',
+			templateUrl: 'modules/articles/views/edit-article.client.view.html'
+		});*/
+	}
+]);
---
+++ public/modules/events/controllers/events.client.controller.js
@@ -0,0 +1,68 @@
+'use strict';
+
+// Articles controller
+angular.module('events').controller('EventsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Articles',
+	function($scope, $stateParams, $location, Authentication, Events) {
+		$scope.authentication = Authentication;
+
+		// Create new Article
+		$scope.create = function() {
+			// Create new Article object
+			var eventmodel = new Events({
+				title: this.title,
+				content: this.content
+			});
+
+			// Redirect after save
+			eventmodel.$save(function(response) {
+				$location.path('events/' + response._id);
+
+				// Clear form fields
+				$scope.title = '';
+				$scope.content = '';
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		// Remove existing Article
+		$scope.remove = function(eventmodel) {
+			if (eventmodel) {
+				eventmodel.$remove();
+
+				for (var i in $scope.eventmodels) {
+					if ($scope.eventmodels[i] === eventmodel) {
+						$scope.eventmodels.splice(i, 1);
+					}
+				}
+			} else {
+				$scope.eventmodel.$remove(function() {
+					$location.path('eventmodels');
+				});
+			}
+		};
+
+		// Update existing Article
+		$scope.update = function() {
+			var eventmodel = $scope.eventmodel;
+
+			eventmodel.$update(function() {
+				$location.path('events/' + eventmodel._id);
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		// Find a list of Articles
+		$scope.find = function() {
+			$scope.eventmodels = Events.query();
+		};
+
+		// Find existing Article
+		$scope.findOne = function() {
+			$scope.eventmodel = Events.get({
+				eventId: $stateParams.eventId
+			});
+		};
+	}
+]);
