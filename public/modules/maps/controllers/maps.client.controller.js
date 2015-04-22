fb5a8a0e94612f7351d8c9686fd07da2bc22c7ef
suprita shankar <suprita.shankar@gmail.com>
2015-04-22 10:47:18 -0700

    last commit

--- public/modules/maps/controllers/maps.client.controller.js
+++ public/modules/maps/controllers/maps.client.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Articles controller
-angular.module('maps').controller('MapsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Events',
-	function($scope, $stateParams, $location, Authentication, Events) {
+angular.module('maps').controller('MapsController', ['$http', '$scope', '$stateParams', '$location', 'Authentication', 'Events',
+	function($http, $scope, $stateParams, $location, Authentication, Events) {
 
  	if (navigator.geolocation) {
          navigator.geolocation.getCurrentPosition(function(position){
@@ -13,8 +13,6 @@ angular.module('maps').controller('MapsController', ['$scope', '$stateParams', '
         });
         }
 		$scope.authentication = Authentication;
-	
-
 		//$scope.events = Events.query();
 		//console.log('I am in events');
 		//$scope.results = Events.eventsnearby(100,200);
@@ -76,8 +74,8 @@ angular.module('maps').controller('MapsController', ['$scope', '$stateParams', '
 									location : [36.703660, 98.964844]
 								}
 								];
-
-    var mapOptions = {
+    $scope.createMap = function(data) {
+	var mapOptions = {
         zoom: 4,
         center: new google.maps.LatLng(40.0000, -98.0000),
         mapTypeId: google.maps.MapTypeId.TERRAIN
@@ -95,13 +93,13 @@ angular.module('maps').controller('MapsController', ['$scope', '$stateParams', '
 
         var marker = new google.maps.Marker({
             map: $scope.map,
-            position: new google.maps.LatLng(info.location[0], info.location[1]),
+            position: new google.maps.LatLng(info.location[1], info.location[0]),
             title: info.title
         });
         marker.content = '<div class="infoWindowContent">' + info.content + '</div>';
 
 				// fit this marker in the map
-				var myLatLng = new google.maps.LatLng(info.location[0], info.location[1]);
+				var myLatLng = new google.maps.LatLng(info.location[1], info.location[0]);
 				latlngbounds.extend(myLatLng);
 
         google.maps.event.addListener(marker, 'click', function(){
@@ -119,15 +117,22 @@ angular.module('maps').controller('MapsController', ['$scope', '$stateParams', '
 
 		$scope.map.setCenter(latlngbounds.getCenter());
 		$scope.map.fitBounds(latlngbounds);
+    };
 
-		var input = document.getElementsByClassName('zipcode-input')[0];
-		var searchBox = new google.maps.places.SearchBox(input);
+    $http.get('http://localhost:3000/nearby?lng=-121.922&lat=37.37').success(function(data){
+			console.log(data);
+			$scope.createMap(data);
+
+	});
 
 
     $scope.openInfoWindow = function(e, selectedMarker){
         e.preventDefault();
         google.maps.event.trigger(selectedMarker, 'click');
     };
+
+		var input = document.getElementsByClassName('zipcode-input')[0];
+		var searchBox = new google.maps.places.SearchBox(input);
 
 		google.maps.event.addListener(searchBox, 'places_changed', function() {
 			var places = searchBox.getPlaces();
@@ -137,9 +142,14 @@ angular.module('maps').controller('MapsController', ['$scope', '$stateParams', '
 			}
 
 			var longitude = places[0].geometry.location.lng(),
-					latitude = places[0].geometry.location.lat();
-			$scope.map.setCenter(new google.maps.LatLng(latitude, longitude));
-			$scope.map.setZoom(12);
+				latitude = places[0].geometry.location.lat();
+			var query = "http://localhost:3000/nearby?lng=" + longitude+ "&lat=" + latitude;
+
+		    $http.get(query).success(function(data){
+					console.log(data);
+					$scope.createMap(data);
+
+			});
 		});
 
 	}
