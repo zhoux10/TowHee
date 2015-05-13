0097c41ff9508e29d6c82233e4c8e7fbe351fa65
Sherry Zhou <5643f56c@opayq.com>
2015-05-13 05:40:11 -0700

    Rename powerpoint file

--- public/modules/articles/config/articles.client.config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// Configuring the Articles module
-angular.module('articles').run(['Menus',
-	function(Menus) {
-		// Set top bar menu items
-	}
-]);
--- public/modules/maps/config/maps.client.config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Configuring the Articles module
-angular.module('maps').run(['Menus',
-	function(Menus) {
-		Menus.addSubMenuItem('topbar', 'maps', 'View Events', '/maps');
-		
-	}
-]);
